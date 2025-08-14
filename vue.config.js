44f533721c01613f6e4da307827b5e37aa237a5c
Aadil86438 <mraadildesktop@gmail.com>
2025-08-14 23:27:36 +0530

    Prepare for GitHub Pages deploy

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,12 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
   // Use the repo name for GitHub Pages project site
   publicPath: process.env.NODE_ENV === 'production' ? '/WelcomeTurf/' : '/',
+  
   transpileDependencies: [
     'vuetify'
-  ]
-}
+  ],
 
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
   lintOnSave: false
 })
